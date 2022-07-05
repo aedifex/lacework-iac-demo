6aea78f8f5efe2e4ec52c6c3a11d922f4fe9eac6
Chris Black <chris.black@lacework.net>
2022-07-04 19:07:50 -0700

    Update primary terraform module

--- terraform/webserver-cluster/main.tf
+++ terraform/webserver-cluster/main.tf
@@ -1,12 +1,10 @@
 terraform {
-  required_version = ">= 0.12, < 0.13"
+  # required_version = ">= 0.12, < 0.13"
 }
 
 provider "aws" {
   region = "us-east-2"
-
-  # Allow any 2.x version of the AWS provider
-  version = "~> 2.0"
+  profile = var.aws_profile_name
 }
 
 resource "aws_launch_configuration" "example" {
