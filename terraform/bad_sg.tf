c62bf10c5f2223427a4f2174d93a3eadf5bb4c42
Chris Black <chris.black@lacework.net>
2022-07-19 12:12:15 -0700

    move file

--- terraform/bad_sg.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_security_group" "bad_example" {
-  name = "a_bad_security_group"
-  ingress {
-    from_port   = 1234
-    to_port     = 1234
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
